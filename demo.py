fc376071393566848cae8e556d04c73815082ed6
sirius2beta <92342848+sirius2beta@users.noreply.github.com>
2023-12-08 11:45:41 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,5 +1,5 @@
-import gcs
+import MavManager
 
-mavrouter = MavRouter()
+mavrouter = MavManager.MavManager()
 mavrouter.connectGCS('udp:192.168.0.99:14450',True)
 mavrouter.connectInput("/dev/ttyACM0")
